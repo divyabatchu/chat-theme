30585be8bedf9e50ce1f1359109f8bdf4d881db2
divya batchu <divyabatchu94@gmail.com>
2021-03-31 15:53:35 +0530

    Ctrl

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import { ChatEngine} from 'react-chat-engine';
+import './App.css';
+
+const App = () => {
+    return(
+        <ChatEngine
+         height="100vh"
+         projectID="60ca0fe8-663a-4246-94ab-96f4da69f592"
+         userName="nikirushi"
+         userSecret="24682468"
+        />
+    );
+}
+export default App;
